c6a401b1d5f439d054a645e0bffdcca4f60515eb
ParraJuanPablo <117546451+ParraJuanPablo@users.noreply.github.com>
2023-06-23 22:30:44 +0000

    Primer Avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,12 +2,133 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
 	const [ inputValue, setInputValue ] = useState("")
 	const [ todos, setTodos ] = useState([])
+	function getTodos(){
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/ParraJuanPablo', {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json"
+		}
+		})
+		.then(resp => {
+			console.log(resp.ok); // will be true if the response is successfull
+			console.log(resp.status); // the status code = 200 or code = 400 etc.
+			console.log(resp.text()); // will try return the exact result as string
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then(data => {
+			//here is where your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+			setTodos(JSON.stringify(data))
+		})
+		.catch(error => {
+			//error handling
+			console.log(error);
+		});
+	};
+	function addPutTodos(a){
+
+		setTodos(todos.concat({
+			"label": a,
+			"done": false
+		  }))
+		
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/ParraJuanPablo', {
+		  method: "PUT",
+		  body: JSON.stringify(todos),
+		  headers: {
+			"Content-Type": "application/json"
+		  }
+		})
+		.then(resp => {
+			console.log(resp.ok); // will be true if the response is successfull
+			console.log(resp.status); // the status code = 200 or code = 400 etc.
+			console.log(resp.text()); // will try return the exact result as string
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then(data => {
+			//here is where your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+		})
+		.catch(error => {
+			//error handling
+			console.log(error);
+		});
+	};
+	function deletePutTodos(t, i){
+
+		setTodos(todos.filter((t, currentI) => i != currentI))
+
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/ParraJuanPablo', {
+		  method: "PUT",
+		  body: JSON.stringify(todos),
+		  headers: {
+			"Content-Type": "application/json"
+		  }
+		})
+		.then(resp => {
+			console.log(resp.ok); // will be true if the response is successfull
+			console.log(resp.status); // the status code = 200 or code = 400 etc.
+			console.log(resp.text()); // will try return the exact result as string
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then(data => {
+			//here is where your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+		})
+		.catch(error => {
+			//error handling
+			console.log(error);
+		});
+	};
+	function emptyPutTodos(){
+
+		setTodos([])
+
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/ParraJuanPablo', {
+		  method: "PUT",
+		  body: JSON.stringify(todos),
+		  headers: {
+			"Content-Type": "application/json"
+		  }
+		})
+		.then(resp => {
+			console.log(resp.ok); // will be true if the response is successfull
+			console.log(resp.status); // the status code = 200 or code = 400 etc.
+			console.log(resp.text()); // will try return the exact result as string
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then(data => {
+			//here is where your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+		})
+		.catch(error => {
+			//error handling
+			console.log(error);
+		});
+	};
+	const Todos = (props) => {
+	
+		// this function useEffect will run only one time, when the component is finally loaded the first time.
+		useEffect(() =>
+			// here I fetch my todos from the API
+			fetch('https://assets.breatheco.de/apis/fake/todos/user/ParraJuanPablo', {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json"
+				}
+				})
+				.then(r => r.json())
+				.then(data => setTodos(data)) // here it re-set the variable tasks with the incoming data
+		, []);
+	
+		return <ul>{todos.map(t => <li className="d-flex justify-content-between"><span><i className="fa-solid fa-chevron-right"></i> {t.label}</span><span className="hide"><i className="fa-solid fa-x float-right exe" onClick={() => deletePutTodos(t, i)} ></i></span></li>)}</ul>;
+	}
 	return (
 		<div className="container">
 			<h1>Todo List</h1>
@@ -19,21 +140,20 @@ const Home = () => {
 						value={inputValue} 
 						onKeyDown={(e) => {
 								if(e.key === "Enter") {
-									setTodos(todos.concat(inputValue));
+									addPutTodos(inputValue);
 									setInputValue("");
 								}
 							}}
 						placeholder="Agrega una Tarea...">
 					</input>
 				</li>
-				{todos.map((t, i) => (
-					<li className="d-flex justify-content-between">
-						<span><i class="fa-solid fa-chevron-right"></i> {t}</span><span className="hide"><i class="fa-solid fa-x float-right exe" onClick={() => setTodos(todos.filter((t, currentI) => i != currentI))} ></i></span>
-					</li>
-				))}
+				{Todos()}
 			</ul>
 			<div className={"noTask"+ (todos.length === 0 ? "" : " hide")}>No hay Tareas, agrega una.</div>
-			<div className="total">{todos.length} Tareas</div>
+			<div className="end">
+				<div className="total">{todos.length} Tareas</div>
+				<div className="trash" onClick={() => emptyPutTodos()}><i className="fa-solid fa-trash-can"></i></div>
+			</div>
 		</div>
 	);
 };
